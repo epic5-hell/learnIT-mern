bb5e04c526e8696388331fe2fd9455f9fd6a5f4b
= <=>
2021-10-17 09:41:41 +0700

    Fix server

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl = 
     process.env.NODE_ENV !== 'production' 
         ? 'http://localhost:5000/api'
-        : 'https://fathomless-fortress-23693.herokuapp.com'
+        : 'https://fathomless-fortress-23693.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
